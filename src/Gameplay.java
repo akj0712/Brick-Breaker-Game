521175eb5744a7b3059f2043eb137f68e8cc7d86
Darklord1o7 <94193086+Darklord1o7@users.noreply.github.com>
2022-01-24 17:07:46 +0530

    Added Gameplay

---
+++ src/Gameplay.java
@@ -0,0 +1,143 @@
+import java.util.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener 
+{
+	private boolean play = false;
+	private int score = 0;
+	
+	private int totalBricks = 48;
+	
+	private Timer timer;
+	private int delay=8;
+	
+	private int playerX = 310;
+	
+	private int ballposX = 120;
+	private int ballposY = 350;
+	private int ballXdir = -1;
+	private int ballYdir = -2;
+	
+	private MapGenerator map;
+	
+	public Gameplay()
+	{		
+		map = new MapGenerator(4, 12);
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+        timer=new Timer(delay,this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g)
+	{    		
+		// background
+		g.setColor(Color.black);
+		g.fillRect(1, 1, 692, 592);
+		
+		// drawing map
+		map.draw((Graphics2D) g);
+		
+		// borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592);
+		
+		// the scores 		
+		g.setColor(Color.white);
+		g.setFont(new Font("serif",Font.BOLD, 25));
+		g.drawString(""+score, 590,30);
+		
+		// the paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		// the ball
+		g.setColor(Color.yellow);
+		g.fillOval(ballposX, ballposY, 20, 20);
+	
+		// when you won the game
+		if(totalBricks <= 0)
+		{
+			 play = false;
+             ballXdir = 0;
+     		 ballYdir = 0;
+             g.setColor(Color.RED);
+             g.setFont(new Font("serif",Font.BOLD, 30));
+             g.drawString("You Won", 260,300);
+             
+             g.setColor(Color.RED);
+             g.setFont(new Font("serif",Font.BOLD, 20));           
+             g.drawString("Press (Enter) to Restart", 230,350);  
+		}
+		
+		// when you lose the game
+		if(ballposY > 570)
+        {
+			 play = false;
+             ballXdir = 0;
+     		 ballYdir = 0;
+             g.setColor(Color.RED);
+             g.setFont(new Font("serif",Font.BOLD, 30));
+             g.drawString("Game Over, Scores: "+score, 190,300);
+             
+             g.setColor(Color.RED);
+             g.setFont(new Font("serif",Font.BOLD, 20));           
+             g.drawString("Press (Enter) to Restart", 230,350);        
+        }
+		
+		g.dispose();
+	}	
+
+	public void keyPressed(KeyEvent e) 
+	{
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{        
+			if(playerX >= 600)
+			{
+				playerX = 600;
+			}
+			else
+			{
+				moveRight();
+			}
+        }
+		
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{          
+			if(playerX < 10)
+			{
+				playerX = 10;
+			}
+			else
+			{
+				moveLeft();
+			}
+        }		
+		if (e.getKeyCode() == KeyEvent.VK_ENTER)
+		{          
+			if(!play)
+			{
+				play = true;
+				ballposX = 120;
+				ballposY = 350;
+				ballXdir = -1;
+				ballYdir = -2;
+				playerX = 310;
+				score = 0;
+				totalBricks = 21;
+				map = new MapGenerator(3, 7);
+				
+				repaint();
+			}
+        }		
+	}
